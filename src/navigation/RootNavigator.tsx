4a570363608c566e232c5456d37ca758b4d06748
Dirk Eisold <deisold@gmail.com>
2025-01-23 17:05:18 +0100

    clean up

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,7 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
-import Test from '../screens/Test';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +9,6 @@ export default function RootNavigator() {
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={HomeScreen} />
-                {/* <Stack.Screen name="Test" component={Test} /> */}
             </Stack.Navigator>
         </NavigationContainer>
     );
